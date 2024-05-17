00e712dc29f486078059dc0c7f8cca43b8d95438
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-05-17 22:54:57 +0530

    Create Notification functions and activities

--- Frontend/asset/js/App.js
+++ Frontend/asset/js/App.js
@@ -5,14 +5,12 @@ let firstName = document.getElementById("firstName");
 let lastName = document.getElementById("lastName");
 let province = document.getElementById("province");
 let age = document.getElementById("age");
-
-let notification = document.getElementById("notification-content");
 let tableBody = document.getElementById("tableBody")
 let searchBox = document.getElementById("searchBox")
 
 function registre() {
-    if (firstName.value == "" || lastName.value == "" || province.value == "Select Province" || age.value== "" || Number(age.value)<=0) {
-        notification.innerHTML = "Please enter valid data"
+    if (firstName.value == "" || lastName.value == "" || province.value == "Select Province" || age.value == "" || Number(age.value) <= 0) {
+        notification("Empty textBox", "Please enter valid data for registration", "warning", "no");
     }
     else {
         let requestBody = {
@@ -30,8 +28,34 @@ function registre() {
             }
         })
             .then(res => res.json())
-            .then(data => notification.innerHTML = data.firstName + " resgistration succesfull");
+            .then(data => { });
+
+        notification("Registration Success", "The student Registration successfully", "success", "yes")
     }
+}
+
+//------------- Nitification functions -------------
+
+function notification(nitifiTitle, notifiText, notifiType, clear) {
+    swal(
+        {
+            title: nitifiTitle,
+            text: notifiText,
+            type: notifiType,
+            confirmButtonColor: "#1bc258",
+            confirmButtonText: "Ok",
+            closeOnConfirm: false,
+            closeOnCancel: false,
+        },
+        function (isConfirm) {
+            if (isConfirm) {
+                if (clear == "yes") {
+                    clearAll();
+                }
+                swal.close();
+            }
+        }
+    );
 }
 
 function clearAll() {
--- Frontend/asset/js/student.js
+++ Frontend/asset/js/student.js
@@ -3,16 +3,18 @@ let firstName = document.getElementById("firstName");
 let lastName = document.getElementById("lastName");
 let province = document.getElementById("province");
 let age = document.getElementById("age");
+let searchBox = document.getElementById("searchBox");
 
 let body = ``;
 
 tableLoad();
 
 function tableLoad() {
+    let body = ``;
     fetch("http://localhost:8080/student")
-        .then(res => res.json())
-        .then(data => {
-            data.forEach(element => {
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach((element) => {
                 body += `<tr>
                     <th>${element.id}</th>
                     <td>${element.firstName}</td>
@@ -20,13 +22,13 @@ function tableLoad() {
                     <td>${element.age}</td>
                     <td>${element.province}</td>
                     <td>
-                        <button type="button" class="btn btn-secondary" onclick="edit(${element.id})" data-bs-toggle="modal" data-bs-target="#updateModel">Edit</button>
-                        <button type="button" class="btn btn-danger" onclick="confirmationOfDelete(${element.id})" data-bs-toggle="modal" data-bs-target="#deleteConfirmation">Delete</button>
+                        <button type="button" class="btn btn-secondary" onclick="edit(${element.id})" data-bs-toggle="modal" data-bs-target="#updateModel" >Edit</button>
+                        <button type="button" class="btn btn-danger" onclick="confirmationOfDelete(${element.id})" data-bs-toggle="modal" data-bs-target="#deleteConfirmation" >Delete</button>
                     </td>
-                </tr>`
+                </tr>`;
             });
             tableBody.innerHTML = body;
-        })
+        });
 }
 
 function search() {
@@ -34,9 +36,9 @@ function search() {
 
     //search by province
     fetch("http://localhost:8080/student-by-province/" + searchBox.value)
-        .then(res => res.json())
-        .then(data => {
-            data.forEach(element => {
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach((element) => {
                 body += `<tr>
                         <th>${element.id}</th>
                         <td>${element.firstName}</td>
@@ -47,16 +49,16 @@ function search() {
                             <button type="button" class="btn btn-secondary" onclick="edit(${element.id})" data-bs-toggle="modal" data-bs-target="#updateModel">Edit</button>
                             <button type="button" class="btn btn-danger" onclick="confirmationOfDelete(${element.id})" data-bs-toggle="modal" data-bs-target="#deleteConfirmation">Delete</button>
                         </td>
-                    </tr>`
+                    </tr>`;
             });
             tableBody.innerHTML = body;
-        })
+        });
 
     //search by first name
     fetch("http://localhost:8080/student-by-firstName/" + searchBox.value)
-        .then(res => res.json())
-        .then(data => {
-            data.forEach(element => {
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach((element) => {
                 body += `<tr>
                         <th>${element.id}</th>
                         <td>${element.firstName}</td>
@@ -67,16 +69,16 @@ function search() {
                             <button type="button" class="btn btn-secondary" onclick="edit(${element.id})" data-bs-toggle="modal" data-bs-target="#updateModel">Edit</button>
                             <button type="button" class="btn btn-danger" onclick="confirmationOfDelete(${element.id})" data-bs-toggle="modal" data-bs-target="#deleteConfirmation">Delete</button>
                         </td>
-                    </tr>`
+                    </tr>`;
             });
             tableBody.innerHTML = body;
-        })
+        });
 
     //search by last name
     fetch("http://localhost:8080/student-by-lastName/" + searchBox.value)
-        .then(res => res.json())
-        .then(data => {
-            data.forEach(element => {
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach((element) => {
                 body += `<tr>
                         <th>${element.id}</th>
                         <td>${element.firstName}</td>
@@ -87,16 +89,16 @@ function search() {
                             <button type="button" class="btn btn-secondary" onclick="edit(${element.id})" data-bs-toggle="modal" data-bs-target="#updateModel">Edit</button>
                             <button type="button" class="btn btn-danger" onclick="confirmationOfDelete(${element.id})" data-bs-toggle="modal" data-bs-target="#deleteConfirmation">Delete</button>
                         </td>
-                    </tr>`
+                    </tr>`;
             });
             tableBody.innerHTML = body;
-        })
+        });
 
     //search by age
     fetch("http://localhost:8080/student-by-age/" + Number(searchBox.value))
-        .then(res => res.json())
-        .then(data => {
-            data.forEach(element => {
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach((element) => {
                 body += `<tr>
                     <th>${element.id}</th>
                     <td>${element.firstName}</td>
@@ -107,62 +109,93 @@ function search() {
                         <button type="button" class="btn btn-secondary" onclick="edit(${element.id})" data-bs-toggle="modal" data-bs-target="#updateModel">Edit</button>
                         <button type="button" class="btn btn-danger" onclick="confirmationOfDelete(${element.id})" data-bs-toggle="modal" data-bs-target="#deleteConfirmation">Delete</button>
                     </td>
-                </tr>`
+                </tr>`;
             });
             tableBody.innerHTML = body;
-        })
+        });
 }
 
-let StudentId = 0;
 
-//Delete functions
+//------------- Delete functions -------------
+
+let StudentId = 0;
 function confirmationOfDelete(id) {
     StudentId = id;
 }
 
-function deleteStudent() {
+function deleteStudent(id) {
     fetch("http://localhost:8080/student?id=" + StudentId, {
         method: "DELETE",
         headers: {
-            "content-type": "application/json"
+            "content-type": "application/json",
         },
-        body: null
+        body: null,
     })
-        .then(res => res.json())
-        .then(data => { })
+        .then((res) => res.json())
+        .then((data) => { });
+
+    // notification
+    notification("Delete Success", "The student permanent delete successfully", "success");
 }
 
 
-//Update functions
+//------------- Update functions -------------
+
 function edit(id) {
     fetch("http://localhost:8080/student-by-id/" + id)
-        .then(res => res.json())
-        .then(data => {
+        .then((res) => res.json())
+        .then((data) => {
             sId.innerHTML = "Student ID : " + data.id;
             firstName.value = data.firstName;
             lastName.value = data.lastName;
             province.value = data.province;
             age.value = data.age;
             StudentId = data.id;
-        })
+        });
 }
 
 function update() {
     let requestBody = {
-        "id": Number(StudentId),
-        "firstName": firstName.value,
-        "lastName": lastName.value,
-        "province": province.value,
-        "age": Number(age.value)
-    }
+        id: Number(StudentId),
+        firstName: firstName.value,
+        lastName: lastName.value,
+        province: province.value,
+        age: Number(age.value),
+    };
 
     fetch("http://localhost:8080/student", {
         method: "PATCH",
         body: JSON.stringify(requestBody),
         headers: {
-            "content-type": "application/json"
-        }
+            "content-type": "application/json",
+        },
     })
-        .then(res => res.json())
-        .then(data => { })
+        .then((res) => res.json())
+        .then((data) => { });
+
+    // notification
+    notification("Update Success", "The student information update successfully", "success")
+}
+
+
+//------------- Nitification functions -------------
+
+function notification(nitifiTitle, notifiText, notifiType) {
+    swal(
+        {
+            title: nitifiTitle,
+            text: notifiText,
+            type: notifiType,
+            confirmButtonColor: "#1bc258",
+            confirmButtonText: "Ok",
+            closeOnConfirm: false,
+            closeOnCancel: false,
+        },
+        function (isConfirm) {
+            if (isConfirm) {
+                tableLoad()
+                swal.close();
+            }
+        }
+    );
 }
